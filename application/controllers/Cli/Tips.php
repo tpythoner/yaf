154cf922ffd36962b354760b425fa3ae9da0017a
tony <714600759@qq.com>
2015-06-15 15:23:07 +0800

    modify Tips

--- application/controllers/Cli/Tips.php
+++ application/controllers/Cli/Tips.php
@@ -1,10 +1,8 @@
 <?php
 /**
- * @name IndexController
- * @author root
- * @desc 默认控制器
- * @see http://www.php.net/manual/en/class.yaf-controller-abstract.php
+ * shell
  */
+
 class Cli_TipsController extends Ctrl_Cli
 {
 	
