9a99828c84918e37fd4d952ab680d5752a731fb8
tony <714600759@qq.com>
2015-06-29 11:50:46 +0800

    modify style

--- application/controllers/Error.php
+++ application/controllers/Error.php
@@ -7,16 +7,17 @@ class ErrorController extends yaf_controller_abstract
 {
 	public function errorAction($exception) 
 	{
-		if(empty($_GET['yafphp_errormsg'])) {
+		if (empty($_GET['yafphp_errormsg'])) {
 			self::page403();
 		}
 		$this->getView()->assign("exception", $exception);
 	}
 
-	static function page403(){
+	static function page403()
+	{
 		header("HTTP/1.1 403 Forbidden");
 		empty($_SERVER['REDIRECT_URL']) && $_SERVER['REDIRECT_URL'] = '/';
-		if(false !== strpos($_SERVER['REDIRECT_URL'], '<')) {
+		if (false !== strpos($_SERVER['REDIRECT_URL'], '<')) {
 			$_SERVER['REDIRECT_URL'] = '/';
 		}
 		exit("<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML 2.0//EN\">
