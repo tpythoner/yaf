e5d4f396c95798cbc636f35fd9bbfe58a18c8c18
tony <714600759@qq.com>
2015-07-11 11:22:27 +0800

    readme.md cli

---
+++ application/controllers/Cli/README.md
@@ -0,0 +1,8 @@
+#命令行参数
+
+php Cli.php request_uri="/cli_tips/tips/name/admin"
+
+
+1、cli目录下的tips文件
+2、tips方法
+3、name参数admin
--- application/controllers/Cli/Tips.php
+++ application/controllers/Cli/Tips.php
@@ -6,9 +6,10 @@
 class Cli_TipsController extends Ctrl_Cli
 {
 	
-	public function updateAction($name = "tony") 
+	public function tipsAction($name = "tony") 
 	{
 		print_r($name);
+		echo "\n";
 		//Tool_Fnc::dump(Yaf_Registry::get("config"));
 		exit('what\'s your name?');
 	}
